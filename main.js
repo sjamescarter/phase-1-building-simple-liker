e61342f490101435c4c5fbdfd7eaa50cff015b38
sjamescarter <stephenjamescarter@gmail.com>
2022-12-17 09:27:43 -0600

    update 1

--- main.js
+++ main.js
@@ -3,7 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeBtns = document.querySelectorAll('.like-glyph')
+like()
 
+function like() {
+  likeBtns.forEach(element => element.addEventListener('click', function(e) {
+    mimicServerCall()
+    .then(function() {
+      if (e.target.textContent === EMPTY_HEART) {
+        e.target.textContent = FULL_HEART
+        e.target.className = "like-glyph activated-heart"
+      } else if (e.target.textContent === FULL_HEART) {
+        e.target.textContent = EMPTY_HEART
+        e.target.className = "like-glyph"
+      }
+    })
+    .catch(function(error) {
+      let reveal = document.querySelector('#modal')
+      let errorMsg = document.querySelector('#modal-message')
+      reveal.className = ""
+      errorMsg.textContent = error
+      setTimeout(() => (reveal.className = "hidden"), 3000)
+    })
+  }))
+}
 
 
 
